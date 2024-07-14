132cbf65a1c51edd5ff658c14fb11eba09c4e040
elBanna00 <moaazibrahim3@gmail.com>
2024-07-14 22:27:10 +0300

    Initial commit

---
+++ src/test/java/com/wterm/runtally/RuntallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wterm.runtally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuntallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
